cbf4ca436626ced3675d0b965c87d83d441d2fa9
Licantropo3392 <denisalbu287@gmail.com>
2025-01-10 21:09:48 +0100

    Enhance shutdown-on-lan script with improved argument descriptions and add home endpoint to display shutdown message

--- shutdown_on_lan/main.py
+++ shutdown_on_lan/main.py
@@ -6,10 +6,17 @@ import argparse
 app = FastAPI()
 
 parser = argparse.ArgumentParser(description="Shutdown On Lan")
-parser.add_argument("--port", type=int, default=8000, help="Port to run the FastAPI app on")
-parser.add_argument("--message", type=str, default="The system will shut down in 5 seconds", help="Shutdown message")
-parser.add_argument("--hide-message", action='store_true', help="Hide shutdown message")
+parser.add_argument("--port", type=int, default=8000, help="Port to run the FastAPI app on (default: 8000)")
+parser.add_argument("--message", type=str, default="The system will shut down in 5 seconds", help="Shutdown message (default: The system will shut down in 5 seconds)")
+parser.add_argument("--hide-message", action='store_true', help="Hide shutdown message if specified")
 args = parser.parse_args()
+
+@app.get("/")
+async def Home():
+    if args.hide_message:
+        return {"message": "Disabled shutdown message"}
+    else:
+        return {"message": args.message}
 
 @app.get("/shutdown")
 async def GetShutdown():
