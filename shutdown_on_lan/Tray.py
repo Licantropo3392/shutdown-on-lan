fc805fcb1a861ddcdeefef2f5f46ec4030b7d356
Licantropo3392 <denisalbu287@gmail.com>
2025-01-12 15:38:58 +0100

    Refactor Tray.py to use customtkinter and CtkScrollableTextDisabled for improved UI and log display

--- shutdown_on_lan/Tray.py
+++ shutdown_on_lan/Tray.py
@@ -2,44 +2,71 @@ import os
 import pystray
 import threading
 import time
-import tkinter as tk
-from tkinter.scrolledtext import ScrolledText
-from shutdown_on_lan.CreateImage import CreateImage
+from PIL import Image, ImageTk
+import customtkinter as tk
+from shutdown_on_lan.CtkScrollableTextDisabled import CtkScrollableTextDisabled
 
 def Init() -> None:
     """ Create the system tray icon """
+
     tray = pystray.Icon("shutdown-on-lan")
-    tray.icon = CreateImage()
+
+    tray.icon = Image.open("python.png")
     tray.menu = pystray.Menu(
         pystray.MenuItem("Show Logs", ShowLogs),
         pystray.MenuItem("Quit", OnQuit)
     )
+
     tray.run()
 
 def OnQuit(tray) -> None:
+    """ Button to close the program from the system tray icon """
+
     tray.stop()
     os._exit(0)
 
 def ShowLogs() -> None:
-    logViewerRoot = tk.Tk()
-    logViewerRoot.title("Log Viewer")
+    """ Show the logs in a window """
 
-    textArea = ScrolledText(logViewerRoot, wrap=tk.WORD, width=100, height=30)
+    logViewerRoot = tk.CTk()
+    
+    logViewerRoot.geometry("600x400")
+    logViewerRoot.title("shutdown-on-lan: Log Viewer")
+
+    textArea = CtkScrollableTextDisabled(logViewerRoot, width=100, height=30)
     textArea.pack(expand=True, fill='both')
 
     logThread = threading.Thread(target=TailLogs, daemon=True, args=(textArea,))
     logThread.start()
 
+    logViewerRoot.protocol("WM_DELETE_WINDOW", lambda: OnClosing(logViewerRoot, logThread))
     logViewerRoot.mainloop()
 
-def TailLogs(textArea: ScrolledText) -> None:
+def TailLogs(textArea: CtkScrollableTextDisabled) -> None:
+    """ Continuously read the log file and update the text in the window """
+
+    global stopTail
+    stopTail = False
+
     with open("server.log", "r") as logFile:
         while True:
+            if stopTail:
+                break
+
             logLine = logFile.readline()
+
             if logLine:
-                textArea.configure(state=tk.NORMAL)
-                textArea.insert(tk.END, logLine)
-                textArea.see(tk.END)
-                textArea.configure(state=tk.DISABLED)
+                textArea.insert(logLine)
             else:
                 time.sleep(0.1)
+
+
+def OnClosing(logViewerRoot: tk.CTk, logThread: threading.Thread) -> None:
+    """ Close the log viewer window """
+
+    global stopTail
+    stopTail = True
+
+    logThread.join()
+    
+    logViewerRoot.destroy()
