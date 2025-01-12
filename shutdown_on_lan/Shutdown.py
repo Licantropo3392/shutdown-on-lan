cddfeea8b67959dab3ae5db43e03eaaa83658715
Licantropo3392 <denisalbu287@gmail.com>
2025-01-12 15:38:50 +0100

    Add time parameter to Shutdown function for customizable shutdown delay

--- shutdown_on_lan/Shutdown.py
+++ shutdown_on_lan/Shutdown.py
@@ -1,8 +1,9 @@
 import os
 
-def Shutdown(message, hide_message) -> None:
+def Shutdown(message, hide_message, time) -> None:
     """ Shutdown the system with a message if specified """
+
     if hide_message:
-        os.system("shutdown /s /t 5")
+        os.system(f"shutdown /s /t {time}")
     else:
-        os.system(f"shutdown /s /t 5 /c \"{message}\"")
+        os.system(f"shutdown /s /t {time} /c \"{message}\"")
